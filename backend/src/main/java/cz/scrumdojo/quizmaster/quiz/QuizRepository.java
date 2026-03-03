cbf3254cb9ee349d91f599c0a5e0f8ffcd747543
Karel Smutny <karel.smutny@outlook.com>
2026-03-03 17:36:07 +0100

    refactor: Replace native query with Spring Data derived method for findByWorkspaceGuid

--- backend/src/main/java/cz/scrumdojo/quizmaster/quiz/QuizRepository.java
+++ backend/src/main/java/cz/scrumdojo/quizmaster/quiz/QuizRepository.java
@@ -3,10 +3,11 @@ package cz.scrumdojo.quizmaster.quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
 
-    @Query(value = "SELECT * FROM quiz WHERE workspace_guid = ?", nativeQuery = true)
-    java.util.List<Quiz> findByWorkspaceGuid(String guid);
+    List<Quiz> findByWorkspaceGuid(String workspaceGuid);
 
     @Query(value = "SELECT COUNT(*) > 0 FROM quiz WHERE ? = ANY(questions)", nativeQuery = true)
     boolean existsQuizWithQuestionId(int questionId);
